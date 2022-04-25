b145696fd4555d02990f5d8702e0c6f9dfba73e3
Dmitri Kovalenko <kdima0802@gmail.com>
2022-04-25 21:22:03 +0300

    GoshaWeb Alpha 2.0

--- script.js
+++ script.js
@@ -7,34 +7,47 @@ let gosha = document.getElementById("Gosha");
 let speakLight = document.getElementById("sp");
 
 let countSett = 1;
+let dsBtn = true;
 
+let camera = new Audio('assets/sounds/speak/blip3.mp3');
 // Function
 // Gosha
+
 gosha.addEventListener("click", function() {
-    gosha.classList.remove("fade-in");
-    gosha.offsetWidth = gosha.offsetWidth;
-    gosha.classList.add("fade-in");
+    if (dsBtn == true) {
+        gosha.classList.remove("fade-in");
+        gosha.offsetWidth = gosha.offsetWidth;
+        gosha.classList.add("fade-in");
 
-    speakLight.style.opacity = "1";
+        dsBtn = false
+        gosha.style.filter = "brightness(50%)";
 
-    let audio = new Audio('assets/sounds/speak/Tisy.mp3');
-    audio.play();
+        speakLight.style.opacity = "1";
 
-    audio.onloadeddata = function() {
-        rty = Math.round(audio.duration) * 1000;
-        setTimeout(function(){
-            speakLight.style.opacity = "0";
-        }, rty);
+        let audio = new Audio('assets/sounds/speak/Tisy.mp3');
+        audio.play();
+
+        audio.onloadeddata = function() {
+            rty = Math.round(audio.duration) * 1000;
+            setTimeout(function(){
+                speakLight.style.opacity = "0";
+                gosha.style.filter = "brightness(100%)";
+                dsBtn = true;
+            }, rty);
+        }
     }
 });
+
 
 // Настройки
 settBtn.addEventListener("click", function () {
     if (countSett == 0) {
         settings.style.opacity = "0";
+		camera.play();
         countSett += 1;
     } else {
         settings.style.opacity = "1";
+		camera.play();
         countSett -= 1;
     }
 });
