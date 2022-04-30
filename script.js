53dc057976c31f8d7649db0a6b0b24430210099a
starfayer <nikita.mikhailikov@mail.ru>
2022-04-30 13:43:58 +0300

    init: add first features in alpha 2.0

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
 // GoshaWeb Alpha 1.0
 
 // Переменные
-let settBtn = document.querySelector("#Settings_Btn");
-let settings = document.getElementById("settings");
-let gosha = document.getElementById("Gosha");
-let speakLight = document.getElementById("sp");
+// Стоит использовать const, если это возможно
+// Это покажет, что переменная не поменяется никак, только будет использоваться.
+const settBtn = document.querySelector("#Settings_Btn");
+const settings = document.getElementById("settings");
+const gosha = document.getElementById("Gosha");
+const speakLight = document.getElementById("speak-light");
 
 let countSett = 1;
 let dsBtn = true;
 
-let camera = new Audio('assets/sounds/speak/blip3.mp3');
+// А почему называется camera?
+const camera = new Audio('assets/sounds/speak/blip3.mp3');
 // Function
 // Gosha
 
 gosha.addEventListener("click", function() {
     if (dsBtn == true) {
-        gosha.classList.remove("fade-in");
-        gosha.offsetWidth = gosha.offsetWidth;
+        // Для чего? 
+        // gosha.classList.remove("fade-in");
+        // gosha.offsetWidth = gosha.offsetWidth;
+
+        // .toggle() будет добавлять класс, если нет, и удалять, если класс есть
         gosha.classList.add("fade-in");
+        // blurElement можно использовать в будущем для разных элементов
+        toggleBlurElement(gosha);
+        toggleHideElement(speakLight);
+        // speakLight.style.opacity = "1";
 
-        dsBtn = false
-        gosha.style.filter = "brightness(50%)";
+        dsBtn = false;
 
-        speakLight.style.opacity = "1";
+        // gosha.style.filter = "brightness(50%)";
 
         let audio = new Audio('assets/sounds/speak/Tisy.mp3');
         audio.play();
 
         audio.onloadeddata = function() {
-            rty = Math.round(audio.duration) * 1000;
-            setTimeout(function(){
-                speakLight.style.opacity = "0";
-                gosha.style.filter = "brightness(100%)";
+            const delay = Math.round(audio.duration) * 1000;
+            setTimeout(function() {
+                toggleBlurElement(gosha);
+                toggleHideElement(speakLight);
+                gosha.classList.remove("fade-in");
                 dsBtn = true;
-            }, rty);
+            }, delay);
         }
     }
 });
 
 
 // Настройки
+let settingsClicked = false;
 settBtn.addEventListener("click", function () {
-    if (countSett == 0) {
-        settings.style.opacity = "0";
-		camera.play();
-        countSett += 1;
-    } else {
-        settings.style.opacity = "1";
-		camera.play();
-        countSett -= 1;
-    }
+  settings.classList.toggle('hide');
+  camera.play();
+  // Вместо подсчёта мы можем пользоваться трюком, который вернет обратное значение
+  // Теперь можно не считать
+
+  // settingsClicked = !settingsClicked;
+  // Можешь применить к dsBtn
+  // if (settingsClicked) {
+  //     settings.classList.add('hide');
+  // } else {
+  //     settings.classList.remove('hide');
+  // }
+  // Но все равно я смог заменить
 });
+
+function toggleBlurElement(el) {
+  el.classList.toggle('blur');
+}
+
+function toggleHideElement(el) {
+  el.classList.toggle('hide');
+}
